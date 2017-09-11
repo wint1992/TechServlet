208abba838a5d577691e0e713b07f31af6da9f7f
a.chuchalin <chuchalin92@mail.ru>
2017-09-12 00:18:10 +0300

    .

--- tech.servlet/src/main/java/ru/chuchalin/tech/servlet/TechApiServlet.java
+++ tech.servlet/src/main/java/ru/chuchalin/tech/servlet/TechApiServlet.java
@@ -13,38 +13,73 @@ import javax.servlet.http.HttpServletResponse;
 import ru.chuchalin.tech.model.dao.GraphQLSchemaModel;
 import ru.chuchalin.tech.model.dao.HibernateUtil;
 
-public class TechApiServlet extends HttpServlet
-{
+public class TechApiServlet extends HttpServlet {
 	private GraphQLSchemaModel ql;
 	private FileWritter file;
-	
-    public static void main( String[] args ) {
-    }
-    
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-    	file = FileWritter.newReletiveFileWritter("logs", "TechApiServlet.log");
-    	file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date()) +")- TechApiServlet.init() Begin");
-    	GraphQLSchemaModel ql = new GraphQLSchemaModel(new HibernateUtil().init("194.87.214.229/TechCalDB", "a.chuchalin", "Achprocedure1@99.2"));
-    	file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date()) +")- TechApiServlet.init() End");
-    	super.init(config);
-    }
-    
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    	resp.setContentType("text/html");
+
+	public static void main(String[] args) {
+		TechApiServlet ts = new TechApiServlet();
+		try {
+			ts.init(null);
+		} catch (Exception e) {
+		}
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		file = FileWritter.newReletiveFileWritter("logs", "TechApiServlet.log");
+		file.write("init");
+		try {
+			file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date())
+					+ ")- TechApiServlet.init() Begin");
+			ql = new GraphQLSchemaModel(
+					new HibernateUtil().init("194.87.214.229/TechCalDB", "a.chuchalin", "Achprocedure1@99.2"));
+			file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date())
+					+ ")- TechApiServlet.init() End");
+		} catch (Exception e) {
+		}
+		// super.init(config);
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String query = req.getParameter("query");
+		if (query != null)
+			file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date()) + ")- Request: "
+					+ query);
+		else
+			file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date())
+					+ ")- Request: null");
+
+		resp.setContentType("text/json");
 		PrintWriter pw = resp.getWriter();
-		file.write(ql.jsonResult("query {city {name}}"));
-		
-		pw.println("<p>" + file.getPath().toString() + "</p>");
-		pw.println("<p>GET</p>");
+		// String queryResult = ql.jsonResult("query" + query);
+		// file.write("INFO-(" +
+		// ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date())
+		// + ")- Response: "
+		// + queryResult);
+		// pw.println(queryResult);
 		pw.close();
-    }
-    
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    	resp.setContentType("text/html");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String query = req.getParameter("query");
+		if (query != null)
+			file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date()) + ")- Request: "
+					+ query);
+		else
+			file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date())
+					+ ")- Request: null");
+
+		resp.setContentType("text/json");
 		PrintWriter pw = resp.getWriter();
-		file.write("POST");
-    }
+		// String queryResult = ql.jsonResult("query" + query);
+		// file.write("INFO-(" +
+		// ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date())
+		// + ")- Response: "
+		// + queryResult);
+		// pw.println(queryResult);
+		pw.close();
+	}
 }
