254df96695bbbe064fc3fe80fab1e20b779cfda3
a.chuchalin <chuchalin92@mail.ru>
2017-09-11 19:13:12 +0300

    update .war

--- tech.servlet/src/main/java/ru/chuchalin/tech/servlet/TechApiServlet.java
+++ tech.servlet/src/main/java/ru/chuchalin/tech/servlet/TechApiServlet.java
@@ -2,6 +2,7 @@ package ru.chuchalin.tech.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -9,15 +10,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import ru.chuchalin.tech.model.dao.GraphQLSchemaModel;
+import ru.chuchalin.tech.model.dao.HibernateUtil;
+
 public class TechApiServlet extends HttpServlet
 {
-	private FileWritter file = FileWritter.newReletiveFileWritter("logs", "TechApiServlet.log");
+	private GraphQLSchemaModel ql;
+	private FileWritter file;
 	
     public static void main( String[] args ) {
     }
     
     @Override
     public void init(ServletConfig config) throws ServletException {
+    	file = FileWritter.newReletiveFileWritter("logs", "TechApiServlet.log");
+    	file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date()) +")- TechApiServlet.init() Begin");
+    	GraphQLSchemaModel ql = new GraphQLSchemaModel(new HibernateUtil().init("194.87.214.229/TechCalDB", "a.chuchalin", "Achprocedure1@99.2"));
+    	file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date()) +")- TechApiServlet.init() End");
     	super.init(config);
     }
     
@@ -25,7 +34,7 @@ public class TechApiServlet extends HttpServlet
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     	resp.setContentType("text/html");
 		PrintWriter pw = resp.getWriter();
-		file.write("GET");
+		file.write(ql.jsonResult("query {city {name}}"));
 		
 		pw.println("<p>" + file.getPath().toString() + "</p>");
 		pw.println("<p>GET</p>");
