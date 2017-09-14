fafdbeec81b33948729bcbf471f5d2adfa7ec8b5
a.chuchalin <chuchalin92@mail.ru>
2017-09-15 00:35:20 +0300

    Servlet v1.0

--- tech.servlet/src/main/java/ru/chuchalin/tech/model/dao/HibernateUtil.java
+++ tech.servlet/src/main/java/ru/chuchalin/tech/model/dao/HibernateUtil.java
@@ -1,59 +1,40 @@
 package ru.chuchalin.tech.model.dao;
 
-import java.nio.file.Paths;
-import java.util.Date;
-
 import org.hibernate.SessionFactory;
-import org.hibernate.service.*;
-
-import ru.chuchalin.tech.servlet.FileWritter;
-
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 	private SessionFactory sessionFactory;
 
-	public HibernateUtil init(String url, String login, String pass, FileWritter file) {
+	public HibernateUtil init(String url, String login, String pass) {
 		HibernateUtil vResult = null;
 		try {
-			file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date()) + ")- HibernateUtil sessionFactory START");
-//			sessionFactory = new Configuration()//
-//					//.setProperties(System.getProperties())
-//					.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver")
-//					.setProperty("hibernate.connection.url", "jdbc:postgresql://" + url)
-//					.setProperty("hibernate.connection.username", login)
-//					.setProperty("hibernate.connection.password", pass)
-//					.setProperty("hibernate.connection.pool_size", "1")//
-//					.setProperty("hibernate.c3p0.min_size", "5")
-//					.setProperty("hibernate.c3p0.max_size", "20").setProperty("hibernate.c3p0.timeout", "500")
-//					.setProperty("hibernate.c3p0.max_statements", "50")//
-//					.setProperty("hibernate.jdbc.batch_size", "10")//
-//					.setProperty("hibernate.jdbc.fetch_size", "100")//
-//					.setProperty("hibernate.id.new_generator_mappings", "true")//
-//					.setProperty("hibernate.connection.release_mode", "after_transaction")//
-//					.setProperty("hibernate.connection.handling_mode", "DELAYED_ACQUISITION_AND_RELEASE_AFTER_TRANSACTION")//
-//					.setProperty("hibernate.current_session_context_class", "thread")//
-//					.addClass(ru.chuchalin.tech.model.City.class)//
-//					.addClass(ru.chuchalin.tech.model.EventMusicStyle.class)//
-//					.addClass(ru.chuchalin.tech.model.EventAddress.class)//
-//					.addClass(ru.chuchalin.tech.model.Profile.class)//
-//					.addClass(ru.chuchalin.tech.model.Event.class)//
-//					.buildSessionFactory();
-//					;
-			sessionFactory = new Configuration().configure().buildSessionFactory();
-			file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date()) + ")- HibernateUtil sessionFactory END");
-			//con.configure();
-			//file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date()) + ")- HibernateUtil Configure END");
-			//sessionFactory = con.buildSessionFactory();
-			//file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date()) + ")- HibernateUtil buildSessionFactory END");
+			sessionFactory = new Configuration()//
+					.setProperties(System.getProperties())
+					.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver")
+					.setProperty("hibernate.connection.url", "jdbc:postgresql://" + url)
+					.setProperty("hibernate.connection.username", login)
+					.setProperty("hibernate.connection.password", pass)
+					.setProperty("hibernate.connection.pool_size", "1")//
+					.setProperty("hibernate.c3p0.min_size", "5").setProperty("hibernate.c3p0.max_size", "20")
+					.setProperty("hibernate.c3p0.timeout", "500").setProperty("hibernate.c3p0.max_statements", "50")//
+					.setProperty("hibernate.jdbc.batch_size", "10")//
+					.setProperty("hibernate.jdbc.fetch_size", "100")//
+					.setProperty("hibernate.id.new_generator_mappings", "true")//
+					.setProperty("hibernate.connection.release_mode", "after_transaction")//
+					.setProperty("hibernate.connection.handling_mode",
+							"DELAYED_ACQUISITION_AND_RELEASE_AFTER_TRANSACTION")//
+					.setProperty("hibernate.current_session_context_class", "thread")//
+					.addClass(ru.chuchalin.tech.model.City.class)//
+					.addClass(ru.chuchalin.tech.model.EventMusicStyle.class)//
+					.addClass(ru.chuchalin.tech.model.EventAddress.class)//
+					.addClass(ru.chuchalin.tech.model.Profile.class)//
+					.addClass(ru.chuchalin.tech.model.Event.class)//
+					.buildSessionFactory();
+			;
 			vResult = this;
-		} catch (Throwable  e) {
-			StringBuffer sb = new StringBuffer();
-			FileWritter.printStackTrace(sb, e);
-			file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date()) + ")- HibernateUtil sessionFactory EXCEPTION: "+ sb.toString());
-			//e.printStackTrace();
+		} catch (Throwable e) {
 			vResult = null;
-			throw e;			
 		}
 		return vResult;
 	}
--- tech.servlet/src/main/java/ru/chuchalin/tech/servlet/TechApiServlet.java
+++ tech.servlet/src/main/java/ru/chuchalin/tech/servlet/TechApiServlet.java
@@ -2,51 +2,38 @@ package ru.chuchalin.tech.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.util.Date;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
 import ru.chuchalin.tech.model.dao.GraphQLSchemaModel;
 import ru.chuchalin.tech.model.dao.HibernateUtil;
 
 public class TechApiServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 	private GraphQLSchemaModel ql;
 	private FileWritter file;
 
 	public static void main(String[] args) {
-		TechApiServlet ts = new TechApiServlet();
-		try {
-			ts.init();
-		} catch (Exception e) {
-		}
 	}
+
 	@Override
 	public void init() throws ServletException {
 		super.init();
 		file = FileWritter.newReletiveFileWritter("logs", "TechApiServlet.log");
-		file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date()) + ")- TechApiServlet.init() Begin");
 		try {
-			file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date()) + ")- HibernateUtil START");
-			ql = new GraphQLSchemaModel(new HibernateUtil().init("194.87.214.229/TechCalDB", "a.chuchalin", "Achprocedure1@99.2", file));
-			file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date()) + ")- GraphQLSchemaModel created");
-			
-			file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date()) + ")- TechApiServlet.init() End");
+			ql = new GraphQLSchemaModel(
+					new HibernateUtil().init("194.87.214.229/TechCalDB", "a.chuchalin", "Achprocedure1@99.2"));
 		} catch (Exception e) {
-			//StringBuffer sb = new StringBuffer();
-			//FileWritter.printStackTrace(sb, e);
-			file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date()) + ")- init() EXCEPTION: " );
+			StringBuffer sb = new StringBuffer();
+			FileWritter.printStackTrace(sb, e);
+			file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date())
+					+ ")- init() EXCEPTION: " + sb.toString());
 
 		}
-		file.write("init END");
 	}
 
 	@Override
@@ -59,10 +46,11 @@ public class TechApiServlet extends HttpServlet {
 			file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date())
 					+ ")- Request: null");
 
-		resp.setContentType("text/json");
+		resp.setContentType("text/json; charset=UTF-8");
 		PrintWriter pw = resp.getWriter();
 		String queryResult = ql.jsonResult("query" + query);
-		file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date()) + ")- Response: " + queryResult);
+		file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date()) + ")- Response: "
+				+ ql.jsonResult("query" + query));
 		pw.println(queryResult);
 		pw.close();
 	}
@@ -77,10 +65,11 @@ public class TechApiServlet extends HttpServlet {
 			file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date())
 					+ ")- Request: null");
 
-		resp.setContentType("text/json");
+		resp.setContentType("text/json; charset=UTF-8");
 		PrintWriter pw = resp.getWriter();
 		String queryResult = ql.jsonResult("query" + query);
-		file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date()) + ")- Response: " + queryResult);
+		file.write("INFO-(" + ru.chuchalin.tech.model.TransformData.transformTimestamp(new Date()) + ")- Response: "
+				+ queryResult);
 		pw.println(queryResult);
 		pw.close();
 	}
